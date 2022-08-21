8577684d0f9d52a11d5dd1b911b4f951e1e7f13a
Chualchai Apichatitiworn <whatbest@MacBook-Pro-khxng-Chualchai.local>
2022-08-21 16:41:18 +0700

    part 5

--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { ImageBackground, Text ,StyleSheet} from 'react-native'
 import Forecast from './Forecast';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Weather(props) {
-    const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
-        temp: 0
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=e9ca12c57259869037bcebb992f9e5c2`)
+        .then((response) => response.json())
+        .then((json) => {
+        setForecastInfo({
+        main: json.weather[0].main,
+        description: json.weather[0].description,
+        temp: json.main.temp
+        });
         })
-    return (
-        <Forecast {...forecastInfo}/>
-    )
-   } 
+        .catch((error) => {
+        console.warn(error);
+        });
+        }
+        }, [props.zipCode])
+        const [forecastInfo, setForecastInfo] = useState({
+            main: '-',
+            description: '-',
+            temp: 0
+            })
+        return (
+            <ImageBackground source={require('../unnamed.jpg')} style={styles.backdrop}>
+                <Text>zipCode</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo}/>
+            </ImageBackground>
+        )
+       }
+       
+    
+    const styles = StyleSheet.create({
+        backdrop: {
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        }
+       });
