85017a9df1f16632b0f6095979d2566956bd18b1
Chualchai Apichatitiworn <whatbest@MacBook-Pro-khxng-Chualchai.local>
2022-08-21 16:33:49 +0700

    part 3

--- wt-app/components/Forecast.js
+++ wt-app/components/Forecast.js
@@ -1,12 +1,14 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+
 export default function Forecast(props) {
     return (
-    <View >
-    <Text>{props.main}</Text>
-    <Text>{props.description}</Text>
     <View>
-    <Text>{props.temp}</Text>
-    <Text>°C</Text>
+        <Text>{props.main}</Text>
+        <Text>{props.description}</Text>
+        <Text>{props.temp}</Text>
+        <Text>°C</Text>
     </View>
-    </View>
-    );
+    )
    }
+
--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Text } from 'react-native'
+import Forecast from './Forecast';
+import { useState } from 'react';
 
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+        })
     return (
-        <Text>{props.zipCode}</Text>
+        <Forecast {...forecastInfo}/>
     )
    } 
